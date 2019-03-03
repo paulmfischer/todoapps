bdf60c78dbfe58041d89a94a7612c5e5119ef8ee
paulmfischer <paul.fischer@ilmservice.com>
2019-03-02 23:17:37 -0600

    Vuejs (#1)
    * add vuejs lib, add vuejs todo implementation
    
    * finalize vuejs implementation
    
    * update ignore file
    
    * remove dot user file

---
+++ todoapps/wwwroot/js/vue.js
@@ -0,0 +1,36 @@
+﻿(() => {
+    let count = 0;
+    Vue.component('todo-item', {
+        props: ['todo'],
+        template: '<div class="row"><div class="col-6"><span>{{todo.name}}</span></div>' +
+            '<div class="col-2"><button :id="getId" v-on:click="$emit(\'remove-todo\', todo.id)" class="btn btn-danger btn-sm" type="button">Remove</button></div></div>',
+        computed: {
+            getId: function () {
+                return `remove-${this.todo.id}`;
+            }
+        }
+    });
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            newTask: '',
+            tasks: []
+        },
+        computed: {
+            isTasksEmpty: function () {
+                return this.tasks.length === 0;
+            }
+        },
+        methods: {
+            addItem: function(event) {
+                event.preventDefault();
+                this.tasks.push({ id: count++, name: this.newTask });
+                this.newTask = '';
+            },
+            removeItem: function (idToRemove) {
+                this.tasks = this.tasks.filter(todo => todo.id !== idToRemove);
+            }
+        }
+    });
+})();
