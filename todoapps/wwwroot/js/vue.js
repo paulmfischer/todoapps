608342a0bf4bd6e20cddfa41002a2b32944eb59b
Paul Fischer <paul.fischer@ilmservice.com>
2019-03-02 23:13:02 -0600

    finalize vuejs implementation

--- todoapps/wwwroot/js/vue.js
+++ todoapps/wwwroot/js/vue.js
@@ -1,8 +1,14 @@
 ﻿(() => {
+    let count = 0;
     Vue.component('todo-item', {
         props: ['todo'],
         template: '<div class="row"><div class="col-6"><span>{{todo.name}}</span></div>' +
-                '<div class="col-2"><button id="remove-{{todo.id}}" class="btn btn-danger btn-sm" type="button">Remove</button></div></div>'
+            '<div class="col-2"><button :id="getId" v-on:click="$emit(\'remove-todo\', todo.id)" class="btn btn-danger btn-sm" type="button">Remove</button></div></div>',
+        computed: {
+            getId: function () {
+                return `remove-${this.todo.id}`;
+            }
+        }
     });
 
     var app = new Vue({
@@ -10,6 +16,21 @@
         data: {
             newTask: '',
             tasks: []
+        },
+        computed: {
+            isTasksEmpty: function () {
+                return this.tasks.length === 0;
+            }
+        },
+        methods: {
+            addItem: function(event) {
+                event.preventDefault();
+                this.tasks.push({ id: count++, name: this.newTask });
+                this.newTask = '';
+            },
+            removeItem: function (idToRemove) {
+                this.tasks = this.tasks.filter(todo => todo.id !== idToRemove);
+            }
         }
     });
 })();
