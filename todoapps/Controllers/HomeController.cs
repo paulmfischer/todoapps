9a78e615d4a0b4b5be826fc06a28f0592d88048b
paulmfischer <paul.fischer@ilmservice.com>
2019-03-06 08:31:28 -0600

    Razor (#2)
    * add razor view todo
    
    * start of delete with razor
    
    * complete delete

--- todoapps/Controllers/HomeController.cs
+++ todoapps/Controllers/HomeController.cs
@@ -25,6 +25,34 @@ namespace todoapps.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Razor()
+        {
+            return View(new ToDoListViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Razor(ToDoListViewModel toDoListViewModel)
+        {
+            var newItem = new ToDoViewModel
+            {
+                Id = toDoListViewModel.ToDos.Count > 0 ? toDoListViewModel.ToDos[toDoListViewModel.ToDos.Count - 1].Id + 1 : 0,
+                Name = toDoListViewModel.NewTask
+            };
+
+            toDoListViewModel.NewTask = string.Empty;
+            toDoListViewModel.ToDos.Add(newItem);
+            return View(toDoListViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult RazorDelete(ToDoListViewModel toDoListViewModel, int Id)
+        {
+            toDoListViewModel.ToDos = toDoListViewModel.ToDos.Where(item => item.Id != Id).ToList();
+
+            return View("Razor", toDoListViewModel);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
